eb08e33bc9b076ef68ffd4eb84243a11b430ca12
IroAlexis <iroalexis@outlook.fr>
2020-11-04 11:37:23 +0100

    [gogextract_zip] Add the documentation

--- gogextract_zip.h
+++ gogextract_zip.h
@@ -12,9 +12,27 @@
 #define SIZE 1024
 
 
+/*!
+ * @brief Convert the file attributes manipulate with libzip in unix file permissions
+ * @param attributes File attributes
+ * @return Unix file permissions
+ **/
 int attr_to_unix_perm(const zip_uint32_t attributes);
 
+
+/*!
+ * @brief Extract only the game since the archive
+ * @param path Archive path
+ * @return 0 if it is a success, -1 otherwise
+ **/
 int extract_game_standalone(const char* path);
 
+
+/*!
+ * @brief Create a directory
+ * @param dir Directory name
+ * @param perm Permission to be assigned
+ * @return 0 if it is success, -1 otherwise
+ **/
 int safe_create_dir(const char* dir, const int perm);
 
